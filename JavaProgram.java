cb4c4d2d58bde1760aba1ca644f99731c2daba9a
venkateshzee <141496661+venkateshzee@users.noreply.github.com>
2023-08-12 16:31:49 +0530

    Update JavaProgram.java

--- JavaProgram.java
+++ JavaProgram.java
@@ -1,5 +1,7 @@
 public class JavaProgram {
     public static void main(String[] args) {
         System.out.println("Hello Java progra ");
+           System.out.println("Hello second line added ");
+        
     }
 }
